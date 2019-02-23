631b23acac3643bb59d6dae5cec094c0968248ae
Tre Shine <t00184855@students.ittralee.ie>
2019-02-23 07:47:08 +0000

    Motion & Enemy

--- Assets/Scripts/SphereControl.cs
+++ Assets/Scripts/SphereControl.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
-public class BallControl : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class SphereControl : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+    
+    
+    float sensitivity_H = 1.0f;
+    float sensitivity_V = 1.0f;
+  
+    float GetAxisH = 0;
+    float GetAxisV = 0;
+    
+    Vector3 baseAcceleration;
+    private Vector3 currentAcceleration;
+    public float speed = 2f; 
+    float smoothness = 0.5f;
+    
+    
+    private Rigidbody rb;
+ 
+
+    
+
+    
+   
+    void ResetAxes(){
+         baseAcceleration = Input.acceleration;
+         currentAcceleration = Vector3.zero;
+     }
+
+     
+         void Start() 
+         {
+             ResetAxes();
+             rb = GetComponent<Rigidbody>();
+         }
+     
+         void Update()
+         {
+             currentAcceleration = Vector3.Lerp(currentAcceleration, Input.acceleration-baseAcceleration, Time.deltaTime/smoothness);
+             GetAxisV = Mathf.Clamp(currentAcceleration.y * sensitivity_V, -1, 1);
+             GetAxisH = Mathf.Clamp(currentAcceleration.x * sensitivity_H, -1, 1);
+             
+             float moveHorizontal = GetAxisH;
+             float moveVertical = GetAxisV;
+           
+             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+             rb.AddForce(movement * speed);
+         }
+         
+         
+       
+         
+     }
